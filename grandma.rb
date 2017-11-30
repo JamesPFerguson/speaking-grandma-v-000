6b54407dab232aff8dbd62e373bc0e4b7ebffd33
James Ferguson <ncjamesferguson@gmail.com>
2017-11-30 17:27:14 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,16 +14,16 @@
 
 def speak_to_grandma(words)
 
-  if words == "I LOVE YOU GRANDMA!"
+  if (words == "I LOVE YOU GRANDMA!")
     puts "I LOVE YOU TOO PUMPKIN"
   end
 
-  elsif words == words.upcase
+  elsif (words == words.upcase)
     puts "NO, NOT SINCE 1938!"
   end
 
   else
     puts "HUH?! SPEAK UP, SONNY!"
   end
-  
+
 end
